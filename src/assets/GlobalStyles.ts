9c4f1e47c0f4121015afa432d1e56dc0dd4bbdf8
MarinaAlPu <marina.pudovkina13@gmail.com>
2026-03-09 21:59:03 +0500

    Add global styles

---
+++ src/assets/GlobalStyles.ts
@@ -0,0 +1,29 @@
+import { createGlobalStyle } from 'styled-components';
+
+
+export const GlobalStyle = createGlobalStyle`
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+  body {
+    font-family: "Roboto", sans-serif;
+  }
+
+  *:before,
+  *:after {
+    box-sizing: border-box;
+  }
+
+  a {
+    text-decoration: none;
+    cursor: pointer;
+  }
+
+  button {
+    cursor: pointer;
+    outline: none;
+  }
+`
