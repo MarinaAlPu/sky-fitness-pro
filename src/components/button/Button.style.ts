43eea8cf829b49b2810a95816363a4992aff2033
MarinaAlPu <marina.pudovkina13@gmail.com>
2026-03-20 10:26:02 +0500

    Add types and styles for button

--- src/components/button/Button.style.ts
+++ src/components/button/Button.style.ts
@@ -1,36 +1,191 @@
 import styled from 'styled-components';
 
 
-export const SButton = styled.button`
-  background-color: #BCEC30;
-  cursor: pointer;
-  border: none;
+export type ButtonType = "primary" | "secondary";
 
-  width: 103px;
+type BackgroundColorType = {
+  regular: string,
+  hover: string,
+  active: string,
+  inactive: string,
+};
+
+type BackgroundColorsType = {
+  primary: BackgroundColorType,
+  secondary: BackgroundColorType,
+};
+
+type ColorType = {
+  regular: string,
+  hover: string,
+  active: string,
+  inactive: string,
+};
+
+type ColorsType = {
+  primary: ColorType,
+  secondary: ColorType,
+};
+
+type SecondaryBorderType = {
+  regular: string,
+  hover: string,
+  active: string,
+  inactive: string,
+};
+
+type BorderType = {
+  primary: string,
+  secondary: SecondaryBorderType,
+};
+
+export type WidthType = {
+  desktop: string;
+  mobile: string;
+};
+
+
+const backgroundColors: BackgroundColorsType = {
+  primary: {
+    regular: "#BCEC30",
+    hover: "#C6FF00",
+    active: "#000000",
+    inactive: "#F7F7F7",
+  },
+  secondary: {
+    regular: "#FFFFFF",
+    hover: "#F7F7F7",
+    active: "#E9ECED",
+    inactive: "#FFFFFF",
+  },
+};
+
+const colors: ColorsType = {
+  primary: {
+    regular: "#000000",
+    hover: "#000000",
+    active: "#FFFFFF",
+    inactive: "#999999",
+  },
+  secondary: {
+    regular: "#000000",
+    hover: "#000000",
+    active: "#000000",
+    inactive: "#999999",
+  },
+};
+
+const borders: BorderType = {
+  primary: "none",
+  secondary: {
+    regular: "1px solid #000000",
+    hover: "1px solid #000000",
+    active: "1px solid #000000",
+    inactive: "1px solid #999999",
+  },
+};
+
+
+type SButtonProps = {
+  $type: ButtonType, //"primary" | "secondary",
+  $width?: string | WidthType,
+  // $borderColor: string,
+  disabled?: boolean,
+  // onClick: () => void
+  // text: string,
+}
+
+
+export const SButton = styled.button<SButtonProps>`
+  /* cursor: pointer; */
+  
   height: 52px;
   border-radius: 46px;
-
+  
   display: flex;
   align-items: center;
   justify-content: center;
-
-  color: #000000;
+  
   font-weight: 400;
   font-size: 18px;
   line-height: 110%;
   letter-spacing: 0px;
   text-align: center;
   
-  &:hover {
-    background-color: #C6FF00;
+  /* width: 103px; */
+  width: ${({ $width }) => {
+    if (typeof $width === "object") {
+      return $width.desktop;
+    } else {
+      return $width || "100%";
+    }
+  }};
+
+/* background-color: #BCEC30; */
+background-color: ${({ $type, disabled }) =>
+    disabled ? backgroundColors[$type].inactive : backgroundColors[$type].regular
+  };
+
+/* color: #000000; */
+color: ${({ $type, disabled }) =>
+    disabled ? colors[$type].inactive : colors[$type].regular
+  };
+
+/* border: none; */
+border: ${({ $type, disabled }) => {
+    if ($type === 'primary') {
+      return borders.primary;
+    } else {
+      return disabled ? borders.secondary.inactive : borders.secondary.regular;
+    }
+  }};
+
+  
+  &:hover:not(:disabled) {
+    /* background-color: #C6FF00; */
+    background-color: ${({ $type }) => backgroundColors[$type].hover};
+    color: ${({ $type }) => colors[$type].hover};
+    border: ${({ $type }) => {
+    if ($type === 'primary') {
+      return borders.primary;
+    } else {
+      return borders.secondary.hover;
+    }
+  }};
+    cursor: pointer;
   }
   
   &:focus {
     outline: none;
   }
 
-  &:active {
-    background-color: #000000;
-    color: #FFFFFF;
+  &:active:not(:disabled) {
+    /* background-color: #000000; */
+    background-color: ${({ $type }) => backgroundColors[$type].active};
+    /* color: #FFFFFF; */
+    color: ${({ $type }) => colors[$type].active};
+    border: ${({ $type }) => {
+    if ($type === 'primary') {
+      return borders.primary;
+    } else {
+      return borders.secondary.active;
+    }
+  }};
+    cursor: pointer;
+  }
+
+  &:disabled {
+    cursor: default;
+  }
+
+
+  @media screen and (max-width: 495px) {
+    width: ${({ $width }) => {
+    if (typeof $width === "object") {
+      return $width.mobile;
+    } else {
+      return $width || "100%";
+    }
+  }};
   }
 `
--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,8 +1,23 @@
-import { SButton } from './Button.style';
+import { SButton, type ButtonType, type WidthType } from './Button.style';
 
 
-export const Button = () => {
-return (
-  <SButton />
-)
+type ButtonProps = {
+  type?: ButtonType,
+  width?: WidthType | string,
+  disabled?: boolean,
+  onClick: () => void,
+  text: string
 }
+
+export const Button: React.FC<ButtonProps> = ({ type = 'primary', width, disabled = false, onClick, text }) => {
+  return (
+    <SButton
+      $type={type}
+      $width={width}
+      disabled={disabled}
+      onClick={onClick}
+    >
+      {text}
+    </SButton>
+  );
+};
