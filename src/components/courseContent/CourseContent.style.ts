204b159112fa1bc6057023fa72a2a4eb08b82132
MarinaAlPu <marina.pudovkina13@gmail.com>
2026-03-22 02:31:08 +0500

    Add course page desktop

--- src/components/courseContent/CourseContent.style.ts
+++ src/components/courseContent/CourseContent.style.ts
@@ -31,6 +31,7 @@ export const SBanner = styled.div`
   background-color: #FFC700;
   position: relative;
   border-radius: 30px;
+  margin-bottom: 60px;
 
   @media screen and (max-width: 375px) {
     
@@ -64,3 +65,205 @@ export const SImage = styled.img`
   z-index: 10;
 `;
 
+export const SCentralBlock = styled.div`
+  width: 100%;
+  /* height: 486px; */
+  display: flex;
+  flex-direction: column;
+  gap: 60px;
+  margin-bottom: 102px;
+`;
+
+export const SCentralBlockDescription = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 40px;
+`;
+
+export const SCentralBlockTitle = styled.div`
+  font-weight: 600;
+  font-size: 40px;
+  line-height: 110%;
+  letter-spacing: 0px;
+`;
+
+export const SDescriptionBlocksWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  gap: 17px;
+`;
+
+export const SDescriptionBlock = styled.div`
+  /* width: 368px;
+  height: 141px; */
+  border-radius: 28px;
+  padding: 20px;
+  background-color: #151720;
+  /* background-color: #1E212E; */
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 25px;
+`;
+
+export const SDescriptionNumber = styled.div`
+  color: #BCEC30;
+  font-weight: 500;
+  font-size: 75px;
+  line-height: 135%;
+  letter-spacing: 0px;
+`;
+
+export const SDescriptionText = styled.div`
+  color: #FFFFFF;
+  font-weight: 400;
+  font-size: 24px;
+  line-height: 110%;
+  letter-spacing: 0px;
+`;
+
+export const SCentralBlockTypes = styled.div`
+  width: 100%;
+  /* height: 486px; */
+  display: flex;
+  flex-direction: column;
+  gap: 40px;
+`;
+
+// export const SDescriptionBlock = styled.div`
+//   /* width: 368px;
+//   height: 141px; */
+//   border-radius: 28px;
+//   padding: 20px;
+//   background-color: #151720;
+//   /* background-color: #1E212E; */
+//   display: flex;
+//   flex-direction: row;
+//   gap: 25px;
+// `;
+
+export const STypesWrapper = styled.div`
+  background-color: #BCEC30;
+  width: 100%;
+  border-radius: 38px;
+  padding: 30px;
+  display: flex;
+  flex-wrap: wrap;
+  column-gap: 124px;
+  row-gap: 34px;
+`;
+
+export const STypesItem = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 284px;
+  gap: 8px;
+`;
+
+export const STypesImage = styled.img`
+  width: 26px;
+  height: 26px;;
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+`;
+
+export const STypesText = styled.div`
+  font-weight: 400;
+  font-size: 24px;
+  line-height: 110%;
+  letter-spacing: 0px;
+`;
+
+export const SBottomBlock = styled.div`
+  width: 100%;
+  height: 486px;
+  padding: 40px;
+  display: flex;
+  flex-direction: row;
+  box-shadow: 0px 4px 67px -12px #00000021;
+  position: relative;
+  /* overflow: hidden; */
+`;
+
+export const SBottomBlockText = styled.div`
+  width: 437px;
+  display: flex;
+  flex-direction: column;
+  gap: 28px;
+`;
+
+export const SBottomBlockTextTitleWrapper = styled.div`
+  width: 400px;
+  /* display: flex; */
+`;
+
+export const SBottomBlockTextTitle = styled.div`
+  font-weight: 500;
+  font-size: 60px;
+  line-height: 100%;
+  letter-spacing: 0px;
+  vertical-align: middle;
+`;
+
+// export const SBottomBlockItems = styled.div`
+//   display: flex;
+//   flex-direction: column;
+// `;
+
+export const SList = styled.ul`
+  list-style: none;
+`;
+
+export const SListItem = styled.li`
+  position: relative;
+  padding-left: 25px;
+  margin-bottom: 10px;
+  font-size: 18px;
+  line-height: 1.4;
+
+  font-weight: 400;
+  font-size: 24px;
+  line-height: 110%;
+  letter-spacing: 0px;
+  vertical-align: middle;
+  opacity: 60%;
+
+  &::before {
+    content: "";
+    position: absolute;
+    left: 0;
+    top: 8px;
+    /* width: 8px;
+    height: 8px; */
+    width: 4.37px;
+    height: 4.06px;
+    background-color: #000000;
+    opacity: 60%;
+    border-radius: 50%;
+  }
+`;
+
+export const SBottomBlockImageWrapper = styled.div`
+  position: absolute;
+  top: -100px;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  overflow: hidden;
+  pointer-events: none;
+`;
+
+export const SBottomBlockImage = styled.img`
+  /* background-image: url('./images/skill-cards/man-lines.png'); */
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+  position: absolute;
+  top: 20px;
+  right: -30px;
+  z-index: 10;
+  height: 690px;
+`;
--- src/components/courseContent/CourseContent.tsx
+++ src/components/courseContent/CourseContent.tsx
@@ -1,7 +1,18 @@
-import { SWrapper, SContainer, SBanner, STitle, SImage } from "./CourseContent.style";
+import { Button } from "../button/Button";
+import {
+  SWrapper, SContainer, SBanner, STitle, SImage, SBottomBlock, SBottomBlockText, SBottomBlockImage, SCentralBlock, SCentralBlockDescription, SCentralBlockTypes, SCentralBlockTitle, SDescriptionBlocksWrapper, SDescriptionBlock, SDescriptionNumber, SDescriptionText, STypesWrapper, STypesItem, STypesImage, STypesText, SBottomBlockImageWrapper, SBottomBlockTextTitle, SList,
+  SListItem,
+  SBottomBlockTextTitleWrapper,
+  //  SBottomBlockItems
+} from "./CourseContent.style";
 
 
 export const CourseContent = () => {
+
+  const handleAddCourse = () => {
+    console.log("Нажали кнопку 'Войдите, чтобы добавить курс'");
+  };
+
 
   return (
     <SWrapper>
@@ -10,6 +21,96 @@ export const CourseContent = () => {
           <STitle>Йога</STitle>
           <SImage src="/images/skill-cards/skill-card-1.svg" alt="Йога" />
         </SBanner>
+
+        <SCentralBlock>
+          <SCentralBlockDescription>
+            <SCentralBlockTitle>Подойдет для вас, если:</SCentralBlockTitle>
+            <SDescriptionBlocksWrapper>
+
+              <SDescriptionBlock>
+                <SDescriptionNumber>1</SDescriptionNumber>
+                <SDescriptionText>Давно хотели попробовать йогу, но не решались начать</SDescriptionText>
+              </SDescriptionBlock>
+
+              <SDescriptionBlock>
+                <SDescriptionNumber>2</SDescriptionNumber>
+                <SDescriptionText>Хотите укрепить позвоночник, избавиться от болей в спине и суставах</SDescriptionText>
+              </SDescriptionBlock>
+
+              <SDescriptionBlock>
+                <SDescriptionNumber>3</SDescriptionNumber>
+                <SDescriptionText>Ищете активность, полезную для тела и души</SDescriptionText>
+              </SDescriptionBlock>
+
+            </SDescriptionBlocksWrapper>
+          </SCentralBlockDescription>
+
+          <SCentralBlockTypes>
+            <SCentralBlockTitle>Направления</SCentralBlockTitle>
+            <STypesWrapper>
+
+              <STypesItem>
+                <STypesImage src="/icons/star.svg" alt="Звёздочка" />
+                <STypesText>Йога для новичков</STypesText>
+              </STypesItem>
+
+              <STypesItem>
+                <STypesImage src="/icons/star.svg" alt="Звёздочка" />
+                <STypesText>Кундалини-йога</STypesText>
+              </STypesItem>
+
+              <STypesItem>
+                <STypesImage src="/icons/star.svg" alt="Звёздочка" />
+                <STypesText>Хатха-йога</STypesText>
+              </STypesItem>
+
+              <STypesItem>
+                <STypesImage src="/icons/star.svg" alt="Звёздочка" />
+                <STypesText>Классическая йога</STypesText>
+              </STypesItem>
+
+              <STypesItem>
+                <STypesImage src="/icons/star.svg" alt="Звёздочка" />
+                <STypesText>Йогатерапия</STypesText>
+              </STypesItem>
+
+              <STypesItem>
+                <STypesImage src="/icons/star.svg" alt="Звёздочка" />
+                <STypesText>Аштанга-йога</STypesText>
+              </STypesItem>
+
+            </STypesWrapper>
+          </SCentralBlockTypes>
+        </SCentralBlock>
+
+        <SBottomBlock>
+          <SBottomBlockText>
+            <SBottomBlockTextTitleWrapper>
+              <SBottomBlockTextTitle>Начните путь к новому телу</SBottomBlockTextTitle>
+            </SBottomBlockTextTitleWrapper>
+
+            {/* <SBottomBlockItems>
+
+            </SBottomBlockItems> */}
+
+            <SList>
+              <SListItem>проработка всех групп мышц</SListItem>
+              <SListItem>тренировка суставов</SListItem>
+              <SListItem>улучшение циркуляции крови</SListItem>
+              <SListItem>упражнения заряжают бодростью</SListItem>
+              <SListItem>помогают противостоять стрессам</SListItem>
+            </SList>
+
+            <Button
+              width={{ desktop: "437px", mobile: "100%" }}
+              onClick={handleAddCourse}
+              text='Войдите, чтобы добавить курс'
+            ></Button>
+          </SBottomBlockText>
+          <SBottomBlockImageWrapper>
+            <SBottomBlockImage src="/images/skill-cards/runner.svg" alt="Мужик" />
+          </SBottomBlockImageWrapper>
+        </SBottomBlock>
       </SContainer>
     </SWrapper>
   )
