e5295bf11c7284a60247b680ae1ae914458daae9
MarinaAlPu <marina.pudovkina13@gmail.com>
2026-03-21 13:34:42 +0500

    Add component Footer + styles

---
+++ src/components/footer/Footer.style.ts
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+
+
+export const SWrapper = styled.div`
+  width: 100%;
+  background-color: #FAFAFA;
+`;
+
+export const SFooterContainer = styled.div`
+  max-width: 1440px;
+  width: 100%;
+  margin: 0 auto;
+  padding: 0 140px 81px 140px;
+
+  @media screen and (max-width: 375px) {
+    padding: 0 16px 29px 16px;
+  }
+`;
+
+export const SFooterBlock = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+
+  @media screen and (max-width: 375px) {
+    justify-content: flex-end;
+  }
+`;
---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,25 @@
+import { SWrapper, SFooterContainer, SFooterBlock } from './Footer.style';
+import { Button } from '../button/Button';
+
+
+export const Footer = () => {
+  const handleUp = () => {
+    console.log('Нажали кнопку "Наверх"');
+  };
+
+
+  return (
+    <SWrapper>
+      <SFooterContainer>
+        <SFooterBlock>
+          <Button
+            // width={{ desktop: "127px", mobile: "127px" }}
+            width="127px"
+            onClick={handleUp}
+            text='Наверх ↑'
+          />
+        </SFooterBlock>
+      </SFooterContainer>
+    </SWrapper>
+  )
+}
