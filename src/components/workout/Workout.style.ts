9e0a078d9075dcd9e4dc22237fcbedcb5391ed59
MarinaAlPu <marina.pudovkina13@gmail.com>
2026-03-23 21:20:43 +0500

    Add workout page

---
+++ src/components/workout/Workout.style.ts
@@ -0,0 +1,148 @@
+import styled from "styled-components";
+
+
+export const SWrapper = styled.div`
+  width: 100%;
+  min-height: 100vh;
+  background-color: #FAFAFA;
+  /* flex-grow: 1; */
+  /* overflow: hidden; */
+  /* margin-top: 120px; */
+  margin-top: 85px;
+`;
+
+export const SContainer = styled.div`
+  width: 100%;
+  max-width: 1440px;
+  min-height: 100vh;
+  margin: 0 auto;
+  padding: 60px 140px 260px 140px;
+  display: flex;
+  flex-direction: column;
+  background-color: #FFFFFF;
+  /* flex-wrap: wrap; */
+  /* position: relative; */
+
+  @media screen and (max-width: 375px) {
+    padding: 24px 16px 40px 16px;
+  }
+`;
+
+export const STitle = styled.div`
+  font-weight: 500;
+  font-size: 60px;
+  line-height: 100%;
+  letter-spacing: 0px;
+  padding-bottom: 40px;
+  
+  @media screen and (max-width: 375px) {
+    font-weight: 500;
+    font-size: 24px;
+    padding-bottom: 24px;
+  }
+`;
+
+export const SVideo = styled.img`
+  width: 1160;
+  height: 639;
+  border-radius: 30px;
+  margin-bottom: 40px;
+  
+  @media screen and (max-width: 375px) {
+    width: 343;
+    height: 189;
+  }
+`;
+
+export const SWorkoutPanel = styled.div`
+  /* width: 1160;
+  height: 639; */
+  border-radius: 30px;
+  padding: 40px;
+  box-shadow: 0px 4px 67px -12px #00000021;
+  display: flex;
+  flex-direction: column;
+  
+  @media screen and (max-width: 375px) {
+    padding: 30px;
+  }
+`;
+
+export const SWorkoutTitle = styled.div`
+  font-weight: 400;
+  font-size: 32px;
+  line-height: 110%;
+  letter-spacing: 0%;
+  border-radius: 30px;
+  box-shadow: 0px 4px 67px -12px #00000021;
+  margin-bottom: 20px;
+  
+  @media screen and (max-width: 375px) {
+    /* font-weight: 400;
+    font-size: 32px; */
+  }
+`;
+
+export const SWorkoutExercises = styled.div`
+  margin-bottom: 40px;
+  /* display: flex; */
+  /* flex-direction: row; */
+  /* flex-wrap: wrap; */
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  column-gap: 60px;
+  row-gap: 20px;
+  
+  @media screen and (max-width: 375px) {
+    /* flex-direction: column; */
+    grid-template-columns: 1fr;
+    gap: 24px;
+  }
+`;
+
+export const SWorkoutExercise = styled.div`
+  /* width: 320px; */
+  /* height: 189; */
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+  
+  @media screen and (max-width: 375px) {
+    /* width: 343;
+    height: 189; */
+  }
+`;
+
+export const SExerciseTitle = styled.div`
+  font-weight: 400;
+  font-size: 18px;
+  line-height: 110%;
+  letter-spacing: 0px;
+  
+  @media screen and (max-width: 375px) {
+    /* width: 343;
+    height: 189; */
+  }
+`;
+
+export const SExerciseProgress = styled.div`
+  width: 100%;
+  height: 6px;
+  border-radius: 50px;
+  background-color: #F7F7F7;
+  border: none;
+  outline: none;
+  
+  @media screen and (max-width: 375px) {
+    /* width: 343;
+    height: 189; */
+  }
+`;
+
+export const SProgressFill = styled.div<{ $percent: number }>`
+  height: 100%;
+  background-color: #00C1FF;
+  border-radius: 50px;
+  
+  width: ${props => props.$percent}%; 
+`;
--- src/components/workout/Workout.tsx
+++ src/components/workout/Workout.tsx
@@ -1,8 +1,80 @@
+import { Button } from "../button/Button";
+import { Header } from "../header/Header";
+import { SContainer, SWrapper, STitle, SVideo, SWorkoutPanel, SWorkoutTitle, SWorkoutExercises, SWorkoutExercise, SExerciseTitle, SExerciseProgress, SProgressFill } from "./Workout.style";
 
 
 export const Workout = () => {
 
+  const progress = 40;
+
+  const handleSetProgress = () => {
+    console.log("Нажали кнопку Заполнить свой прогресс");
+  }
+
   return (
-    <>Страница тренировки</>
+    <>
+      <Header />
+      <SWrapper>
+        <SContainer>
+          <STitle>Йога</STitle>
+          <SVideo src="/images/video.png" alt="Видео" />
+
+          <SWorkoutPanel>
+            <SWorkoutTitle>Упражнения тренировки 2</SWorkoutTitle>
+
+            <SWorkoutExercises>
+              <SWorkoutExercise>
+                <SExerciseTitle>Наклоны вперёд {progress}%</SExerciseTitle>
+                <SExerciseProgress>
+                  <SProgressFill $percent={progress} />
+                </SExerciseProgress>
+              </SWorkoutExercise>
+
+              <SWorkoutExercise>
+                <SExerciseTitle>Наклоны вперёд {progress}%</SExerciseTitle>
+                <SExerciseProgress>
+                  <SProgressFill $percent={progress} />
+                </SExerciseProgress>
+              </SWorkoutExercise>
+
+              <SWorkoutExercise>
+                <SExerciseTitle>Наклоны вперёд {progress}%</SExerciseTitle>
+                <SExerciseProgress>
+                  <SProgressFill $percent={progress} />
+                </SExerciseProgress>
+              </SWorkoutExercise>
+
+              <SWorkoutExercise>
+                <SExerciseTitle>Поднятие ног, согнутых в коленях {progress}%</SExerciseTitle>
+                <SExerciseProgress>
+                  <SProgressFill $percent={progress} />
+                </SExerciseProgress>
+              </SWorkoutExercise>
+
+              <SWorkoutExercise>
+                <SExerciseTitle>Поднятие ног, согнутых в коленях {progress}%</SExerciseTitle>
+                <SExerciseProgress>
+                  <SProgressFill $percent={progress} />
+                </SExerciseProgress>
+              </SWorkoutExercise>
+
+              <SWorkoutExercise>
+                <SExerciseTitle>Поднятие ног, согнутых в коленях {progress}%</SExerciseTitle>
+                <SExerciseProgress>
+                  <SProgressFill $percent={progress} />
+                </SExerciseProgress>
+              </SWorkoutExercise>
+            </SWorkoutExercises>
+
+            <Button
+              width={{desktop: "320px", mobile: "100%"}}
+              onClick={handleSetProgress}
+            >
+              Заполнить свой прогресс
+            </Button>
+          </SWorkoutPanel>
+        </SContainer>
+      </SWrapper>
+    </>
   )
 }
