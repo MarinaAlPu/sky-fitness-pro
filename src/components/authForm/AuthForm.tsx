1b39a0aa13cd0a544785039031fedc6f8e81b747
MarinaAlPu <marina.pudovkina13@gmail.com>
2026-03-29 22:11:27 +0500

    Add userData, handleChange, to AuthForm, add values to inputs

--- src/components/authForm/AuthForm.tsx
+++ src/components/authForm/AuthForm.tsx
@@ -4,8 +4,8 @@ import { SHeaderLogo } from "../header/Header.style";
 import { Input } from "../input/Input";
 import { SPageBackground, SButtonBlock, SInputBlock, SWrapper, SError, SForm } from "./AuthForm.style";
 import { useLocation, useNavigate } from "react-router-dom";
-import { login, registration } from "../../services/auth";
-import axios from "axios";
+// import { login, registration } from "../../services/auth";
+// import axios from "axios";
 import { AuthContext, useAuth } from "../../context/AuthContext";
 
 
@@ -14,11 +14,13 @@ type AuthFormProps = {
 };
 
 
-export const AuthForm = (({ isLogin }: AuthFormProps) => {
+export const AuthForm = ({ isLogin }: AuthFormProps) => {
   const navigate = useNavigate();
   // const location = useLocation();
 
   const {
+    userData,
+    handleChange,
     errorMessage,
     handleOpenRegistrationForm,
     handleOpenLoginForm,
@@ -50,103 +52,6 @@ export const AuthForm = (({ isLogin }: AuthFormProps) => {
   }, [navigate]);
 
 
-  const onChange = () => {
-    console.log("Ввели символ в инпут");
-  };
-
-  // const handleOpenRegistrationForm = () => {
-  //   setErrorMessage("");
-  //   console.log("Нажали кнопку Зарегистрироваться в форме входа");
-  //   // e.preventDefault();
-  //   const parentPath = location.pathname.replace(/\/(login|registration)$/, "");
-  //   // console.log("parentPath: ", parentPath);
-  //   // navigate("/registration");
-  //   navigate(`${parentPath}/registration`);
-  // };
-
-  // const handleOpenLoginForm = () => {
-  //   console.log("Нажали кнопку Войти в форме регистрации");
-  //   setErrorMessage("");
-  //   // e.preventDefault();
-  //   const parentPath = location.pathname.replace(/\/(login|registration)$/, "");
-  //   // navigate("/login");
-  //   // console.log("parentPath: ", parentPath);
-  //   navigate(`${parentPath}/login`);
-  // };
-
-  // const handleLogin = async (e?: React.MouseEvent) => {
-  //   console.log("Нажали кнопку Войти в форме входа");
-
-  //   e?.preventDefault();
-
-  //   const userData = {
-  //     email: "diplom-user2026-10@user.user",
-  //     password: "Parol+-"
-  //   };
-
-  //   const userName = userData.email.split("@")[0];
-  //   console.log("userName: ", userName);
-
-  //   try {
-  //     const data = await login(userData)
-  //     console.log("Ответ сервера при входе: ", data);
-  //     console.log("Токен: ", data.token);
-
-  //     localStorage.setItem("userName", userName);
-  //     localStorage.setItem("email", userData.email);
-  //     localStorage.setItem("token", data.token);
-
-
-  //     const parentPath = window.location.pathname.replace(/\/(login|registration)$/, "");
-  //     navigate(parentPath || "/");
-  //   } catch (error) {
-  //     // console.log("Ошибка при входе: ", error);
-
-  //     if (axios.isAxiosError(error)) {
-  //       if (error && error.response) {
-  //         console.log("Ошибка при регистрации: ", error.response.data.message);
-  //         setErrorMessage(error.response.data.message || "Ошибка регистрации");
-  //       } else if (error.request) {
-  //         setErrorMessage("Отсутствует интернет. Попробуйте позже");
-  //       } else {
-  //         setErrorMessage("Неизвестная ошибка");
-  //       }
-  //     }
-  //   }
-  // };
-
-  // const handleRegister = async (e?: React.MouseEvent) => {
-  //   console.log("Нажали кнопку Зарегистироваться в форме регистрации");
-
-  //   e?.preventDefault();
-
-  //   try {
-  //     const data = await registration({
-  //       email: "diplom-user2026-10@user.user",
-  //       password: "Parol+-"
-  //     })
-  //     console.log("Ответ сервера при регистрации: ", data);
-
-  //     const parentPath = window.location.pathname.replace(/\/(login|registration)$/, "");
-  //     navigate(parentPath || "/");
-  //   } catch (error) {
-  //     // console.log("Ошибка при регистрации: ", error.message);
-  //     // setErrorMessage(error.message)
-
-  //     if (axios.isAxiosError(error)) {
-  //       if (error && error.response) {
-  //         console.log("Ошибка при регистрации: ", error.response.data.message);
-  //         setErrorMessage(error.response.data.message || "Ошибка регистрации");
-  //       } else if (error.request) {
-  //         setErrorMessage("Отсутствует интернет. Попробуйте позже");
-  //       } else {
-  //         setErrorMessage("Неизвестная ошибка");
-  //       }
-  //     }
-  //   }
-  // };
-
-
   return (
     <SPageBackground>
       <SWrapper
@@ -164,19 +69,25 @@ export const AuthForm = (({ isLogin }: AuthFormProps) => {
 
             <Input
               type="email"
-              onChange={onChange}
+              onChange={handleChange}
               placeholder={isLogin ? "Логин" : "Эл. почта"}
+              name="email"
+              value={userData.email}
             />
             <Input
               type="password"
-              onChange={onChange}
+              onChange={handleChange}
               placeholder="Пароль"
+              name="password"
+              value={userData.password} 
             />
             {!isLogin &&
               <Input
                 type="password"
-                onChange={onChange}
+                onChange={handleChange}
                 placeholder="Повторите пароль"
+                name="confirmPassword"
+                value={userData.confirmPassword || ""} 
               />
             }
 
@@ -228,4 +139,4 @@ export const AuthForm = (({ isLogin }: AuthFormProps) => {
       </SWrapper>
     </SPageBackground>
   )
-})
+}
