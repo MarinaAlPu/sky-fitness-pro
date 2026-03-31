36bd04b2e172fbe8859ce249057516e886e78339
MarinaAlPu <marina.pudovkina13@gmail.com>
2026-03-31 20:48:39 +0500

    Add functions addCourse, deleteCourse

--- src/services/courses.ts
+++ src/services/courses.ts
@@ -8,7 +8,40 @@ const API_URL = "https://wedev-api.sky.pro/api/fitness";
 export const fetchCourses = (): Promise<CourseType[]> => {
   return axios.get<CourseType[]>(`${API_URL}/courses`)
     .then((resp) => {
-      console.log("resp в функции fetchCourses: ", resp);
+      // console.log("resp в функции fetchCourses: ", resp);
       return resp.data;
     });
 };
+
+export const addCourse = (id: string, token: string): Promise<any> => {
+  return axios.post(`${API_URL}/users/me/courses`,
+    {
+      courseId: id,
+    }
+    ,
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": ""
+      },
+    })
+    .then((resp) => {
+      console.log("resp в функции addCourse: ", resp);
+      return resp.data;
+    });
+};
+
+export const deleteCourse = (id: string, token: string): Promise<any> => {
+  return axios.delete(`${API_URL}/users/me/courses/${id}`,
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": ""
+      },
+    })
+    .then((resp) => {
+      console.log("resp в функции deleteCourse: ", resp);
+      return resp.data;
+    });
+};
+
