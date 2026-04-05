0e31ac88d37139197c00c81f766e1dbadc72e5a9
MarinaAlPu <marina.pudovkina13@gmail.com>
2026-04-05 14:23:36 +0500

    Add getting exercises progress on workout page, setting exercises progress in ProgressModal

--- src/components/modals/progressModal/ProgressModal.tsx
+++ src/components/modals/progressModal/ProgressModal.tsx
@@ -1,77 +1,83 @@
+import { useState } from "react";
+import { useCourses } from "../../../context/CoursesContext";
 import { Button } from "../../button/Button";
 import { Input } from "../../input/Input";
-import { SPageBackground, SWrapper, STitle, SContent, SItem, SItemTitle } from "./ProgressModal.style";
+import { SPageBackground, SWrapper, STitle, SContent, SItem, SItemTitle, SCloseButton } from "./ProgressModal.style";
+import { useParams } from "react-router-dom";
+import { useAuth } from "../../../context/AuthContext";
 
 
-export const ProgressModal = () => {
-  const onSave = () => {
-    console.log("Нажали кнопку Сохранить");
+type ProgressModalProps = {
+  onCloseModal: () => void;
+};
+
+
+export const ProgressModal = ({ onCloseModal }: ProgressModalProps) => {
+  const { id: workoutId } = useParams();
+  const { token } = useAuth();
+  const { workout, currentCourseId, saveProgress, getUserWorkoutProgress } = useCourses();
+
+  const [exerciseCount, setExerciseCount] = useState<{ [key: number]: number }>({});
+
+  // const onChange = () => {
+  //   console.log("Ввели символ в инпут");
+  // };
+
+  const handleInputChange = (index: number, exerciseCount: string) => {
+    const numValue = exerciseCount === "" ? 0 : Number(exerciseCount);
+
+    setExerciseCount((prev) => ({ ...prev, [index]: numValue }));
   };
 
-  const onChange = () => {
-    console.log("Ввели символ в инпут");
+  const onSave = async () => {
+    console.log("Нажали кнопку Сохранить");
+
+    console.log("currentCourseId в onSave:", currentCourseId);
+    console.log("workoutId в onSave:", workoutId);
+
+    if (!workoutId || !token || !currentCourseId) {
+      return;
+    }
+
+    const progressData = workout.exercises.map((_, index) => {
+      return exerciseCount[index] || 0;
+    });
+
+    console.log("Массив для отправки на сервер:", progressData);
+
+    const resp = await saveProgress(currentCourseId, workoutId, token, progressData);
+
+    console.log(`Сохранили прогресс по тренировке "${workoutId}":`, resp);
+    
+    const respProgress = await getUserWorkoutProgress(currentCourseId, workoutId, token);
+
+    console.log(`Обновили прогресс по тренировке "${workoutId}":`, respProgress);
+
+    onCloseModal();
   };
 
 
   return (
-    <SPageBackground>
-      <SWrapper>
+    <SPageBackground onClick={onCloseModal}>
+      <SWrapper onClick={(e) => e.stopPropagation()}>
         <STitle>Мой прогресс</STitle>
         <SContent>
+          <SCloseButton title="Закрыть" onClick={onCloseModal}>&times;</SCloseButton>
 
-          <SItem>
-            <SItemTitle>Сколько раз вы сделали наклоны вперед?</SItemTitle>
-            <Input
-              type="number"
-              onChange={onChange}
-              height={{desktop: "52px", mobile: "47px"}}
-            />
-          </SItem>
-
-          <SItem>
-            <SItemTitle>Сколько раз вы сделали наклоны назад?</SItemTitle>
-            <Input
-              type="number"
-              onChange={onChange}
-              height={{desktop: "52px", mobile: "47px"}}
-            />
-          </SItem>
-
-          <SItem>
-            <SItemTitle>Сколько раз вы сделали наклоны назад?</SItemTitle>
-            <Input
-              type="number"
-              onChange={onChange}
-              height={{desktop: "52px", mobile: "47px"}}
-            />
-          </SItem>
-
-          <SItem>
-            <SItemTitle>Сколько раз вы сделали поднятие ног, согнутых в коленях?</SItemTitle>
-            <Input
-              type="number"
-              onChange={onChange}
-              height={{desktop: "52px", mobile: "47px"}}
-            />
-          </SItem>
-
-          <SItem>
-            <SItemTitle>Сколько раз вы сделали поднятие ног, согнутых в коленях?</SItemTitle>
-            <Input
-              type="number"
-              onChange={onChange}
-              height={{desktop: "52px", mobile: "47px"}}
-            />
-          </SItem>
-
-          <SItem>
-            <SItemTitle>Сколько раз вы сделали поднятие ног, согнутых в коленях?</SItemTitle>
-            <Input
-              type="number"
-              onChange={onChange}
-              height={{desktop: "52px", mobile: "47px"}}
-            />
-          </SItem>
+          {workout?.exercises?.map((exercise: any, index: number) => (
+            <SItem key={exercise._id}>
+              {/* <SItemTitle>Сколько раз вы сделали наклоны вперед?</SItemTitle> */}
+              <SItemTitle>Сколько раз вы сделали {exercise.name.split(" (")[0]}?</SItemTitle>
+              <Input
+                type="number"
+                value={exerciseCount[index] !== undefined ? String(exerciseCount[index]) : ""}
+                onChange={(e) => handleInputChange(index, e.target.value)}
+                height={{ desktop: "52px", mobile: "47px" }}
+                placeholder="0"
+                name="value"
+              />
+            </SItem>
+          ))}
 
         </SContent>
         <Button
--- src/components/workout/Workout.tsx
+++ src/components/workout/Workout.tsx
@@ -3,17 +3,23 @@ import { useCourses } from "../../context/CoursesContext";
 import { Button } from "../button/Button";
 import { Header } from "../header/Header";
 import { SContainer, SWrapper, STitle, SVideo, SWorkoutPanel, SWorkoutTitle, SWorkoutExercises, SWorkoutExercise, SExerciseTitle, SExerciseProgress, SProgressFill } from "./Workout.style";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useAuth } from "../../context/AuthContext";
+import { getCourseProgress } from "../../services/courses";
+import { ProgressModal } from "../modals/progressModal/ProgressModal";
+import { MessageModal } from "../modals/messageModal/MessageModal";
 
 
 export const Workout = () => {
   const { id } = useParams();
   const { token } = useAuth();
 
-  const { getWorkoutData, workout, getCourses, courses, currentCourseName } = useCourses();
+  const { getWorkoutData, workout, getCourses, courses, currentCourseName, currentCourseId, courseProgress, workoutProgress, getUserCourseProgress, getUserWorkoutProgress } = useCourses();
 
-  // console.log("workout: ", workout);
+  console.log("workout в Workout до useEffect: ", workout);
+  console.log("currentCourseId в Workout до useEffect: ", currentCourseId);
+
+  const [isProgressModalOpen, setIsProgressModalOpen] = useState<boolean>(false);
 
 
   useEffect(() => {
@@ -26,17 +32,38 @@ export const Workout = () => {
     }
   }, [id, token]);
 
+  useEffect(() => {
+    if (currentCourseId && token) {
+      getUserCourseProgress(currentCourseId, token);
+    }
 
-  const progress = 40;
+  }, [currentCourseId, token]);
+
+  useEffect(() => {
+    if (currentCourseId && id && token) {
+      getUserWorkoutProgress(currentCourseId, id, token);
+    }
+
+  }, [currentCourseId, id, token]);
 
 
-  const handleSetProgress = () => {
-    console.log("Нажали кнопку Заполнить свой прогресс");
+  console.log("courseProgress в Workout после useEffect: ", courseProgress);
+  console.log("workoutProgress в Workout после useEffect: ", workoutProgress);
+
+  // const handleSetProgress = () => {
+  //   console.log("Нажали кнопку Заполнить свой прогресс");
+  // };
+
+  const handleOpenProgressModal = () => {
+    setIsProgressModalOpen(true);
+  };
+
+  const onCloseModal = () => {
+    setIsProgressModalOpen(false);
   };
 
 
   // личинка лоадера
-  console.log("workout: ", workout);
   if (!workout) {
     return (
       <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
@@ -70,29 +97,52 @@ export const Workout = () => {
             }</SWorkoutTitle>
 
             <SWorkoutExercises>
-              {workout.exercises.map((exercise) => (
-                <SWorkoutExercise key={exercise._id}>
-                  <SExerciseTitle>
-                    {
-                      exercise.name.split(" (")[0]
-                    } {progress}%
-                  </SExerciseTitle>
-                  <SExerciseProgress>
-                    <SProgressFill $percent={progress} />
-                  </SExerciseProgress>
-                </SWorkoutExercise>
-              ))}
+              {workout.exercises.map((exercise, index) => {
+                // const progress = 40;
+
+                // const currentWorkoutProgress = (courseProgress as any)?.workoutsProgress?.find((item: any) => item.workoutId === id);
+
+                // const currentWorkoutProgress = (workoutProgress as any)?.progressData?.[index] || 0;
+                const currentWorkoutProgress = workoutProgress?.progressData?.[index] || 0;
+
+                // const exerciseCount = currentWorkoutProgress?.progressData?.[index] || 0;
+
+                // const currentProgress = exercise.quantity > 0 ? (exerciseCount/exercise.quantity)*100 : 0;
+
+                const currentProgress = exercise.quantity > 0 ? (currentWorkoutProgress/exercise.quantity)*100 : 0;
+                
+                return (
+                  <SWorkoutExercise key={exercise._id}>
+                    <SExerciseTitle>
+                      {
+                        exercise.name.split(" (")[0]
+                      } {currentProgress}%
+                    </SExerciseTitle>
+                    <SExerciseProgress>
+                      <SProgressFill $percent={currentProgress} />
+                    </SExerciseProgress>
+                  </SWorkoutExercise>
+                )
+              }
+
+              )}
             </SWorkoutExercises>
 
             <Button
               width={{ desktop: "320px", mobile: "100%" }}
-              onClick={handleSetProgress}
+              onClick={handleOpenProgressModal}
             >
               Заполнить свой прогресс
             </Button>
           </SWorkoutPanel>
         </SContainer>
       </SWrapper>
+      {isProgressModalOpen && (<ProgressModal
+      onCloseModal={onCloseModal}
+      // workoutId={activeWorkoutId}
+      // onOpenProgressModal={onOpenProgressModal}
+      />)}
+      {/* {<MessageModal />} */}
     </>
   )
 }
