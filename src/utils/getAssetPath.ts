1a73bc7df166e63b0b2f08aeb5e13e5d0c13290d
MarinaAlPu <marina.pudovkina13@gmail.com>
2026-04-11 14:22:49 +0500

    Add function for getting path for images

---
+++ src/utils/getAssetPath.ts
@@ -0,0 +1,11 @@
+export const getAssetPath = (path: string): string => {
+  let baseUrl = '/';
+
+  try {
+    baseUrl = eval('import.meta.env.BASE_URL');
+  } catch (e) {
+    baseUrl = '/';
+  }
+
+  return `${baseUrl}${path}`;
+};
